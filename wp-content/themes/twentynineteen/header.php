f4d1a02d5164b65b301ff3505a9ae9b034f4b8b6
admin <heiko.valli@khk.ee>
2019-03-05 09:46:42 +0200

    modified theme `Twenty Nineteen` version 1.3

--- wp-content/themes/twentynineteen/header.php
+++ wp-content/themes/twentynineteen/header.php
@@ -20,10 +20,10 @@
 </head>
 
 <body <?php body_class(); ?>>
+		<header id="masthead" class="<?php echo is_singular() && twentynineteen_can_show_post_thumbnail() ? 'site-header featured-image' : 'site-header'; ?>">
+
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'twentynineteen' ); ?></a>
-
-		<header id="masthead" class="<?php echo is_singular() && twentynineteen_can_show_post_thumbnail() ? 'site-header featured-image' : 'site-header'; ?>">
 
 			<div class="site-branding-container">
 				<?php get_template_part( 'template-parts/header/site', 'branding' ); ?>
