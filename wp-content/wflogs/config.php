a2d2a204e04d296600589e1320ec6e3c14258829
admin <heiko.valli@khk.ee>
2019-03-06 12:01:57 +0200

    modified file `config.php`

--- wp-content/wflogs/config.php
+++ wp-content/wflogs/config.php
@@ -3,4 +3,4 @@
 This file is used by the Wordfence Web Application Firewall. Read 
 more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
 ******************************************************************
-a:4:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1552471041;s:7:"authKey";s:64:"c{oWSLuvb*)T&mY~}>J`6EYO~E;gX+>=?xrO$g. P1)jjl^KQ=<Yp!mAfTR$PCPE";}
+a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1552471041;s:7:"authKey";s:64:"c{oWSLuvb*)T&mY~}>J`6EYO~E;gX+>=?xrO$g. P1)jjl^KQ=<Yp!mAfTR$PCPE";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:2554;}
